59edf9414646e4c5b4dc0ea683c1d38bdef37b4e
levi-codeblade-ackerman <levi.codeblade.ackerman@gmail.com>
2026-01-19 08:53:21 -0500

    update: server configuration

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,23 @@
 module.exports = {
-  apps: [{
-    name: 'a2p',
-    script: './build/index.js',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3005,
-      HOST: '0.0.0.0'
-    }
-  }]
+	apps: [
+		{
+			name: 'lead-grabber-v1',
+			script: './build/index.js',
+			instances: 1,
+			exec_mode: 'fork',
+			env: {
+				NODE_ENV: 'production',
+				PORT: 3005,
+				HOST: '0.0.0.0'
+			},
+			error_file: './logs/pm2-error.log',
+			out_file: './logs/pm2-out.log',
+			log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+			merge_logs: true,
+			autorestart: true,
+			max_restarts: 10,
+			min_uptime: '10s',
+			watch: false
+		}
+	]
 };
